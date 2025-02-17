56e82cbb56f09e6fd498ff595369bc6136134275
jaeminSmartel <129919643+jaeminSmartel@users.noreply.github.com>
2025-02-17 15:32:42 +0900

    fix : searchParams type

--- src/app/error/page.tsx
+++ src/app/error/page.tsx
@@ -1,7 +1,7 @@
 import {use} from "react";
 export default function ErrorPage(props : {searchParams: any }) {
     const searchParams = use(props.searchParams)
-    const message = searchParams.message
+    const message = searchParams.message || 'No message provided'
 
     return <p className={'text-txt-01 text-center pt-10'}>Sorry, something went wrong <br/>{message}</p>
 }
